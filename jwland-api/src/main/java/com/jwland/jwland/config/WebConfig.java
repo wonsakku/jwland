0dd0e731ed96c4a4c68ddfe28873ecc2f5eeab7c
wonsakku <kws1044@gmail.com>
2023-04-02 18:00:00 +0900

    cors 설정 추가

---
+++ jwland-api/src/main/java/com/jwland/jwland/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.jwland.jwland.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private final String UI_SERVER;
+
+    public WebConfig(@Value("${ui-server}") String UI_SERVER) {
+        this.UI_SERVER = UI_SERVER;
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins(UI_SERVER)
+//                .allowedMethods("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                ).allowCredentials(false)
+                .maxAge(5000);
+    }
+
+}
