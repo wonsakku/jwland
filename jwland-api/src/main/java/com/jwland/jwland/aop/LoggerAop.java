9208ffeaa657b8329c01d97d4c206754955f1e9f
wonsakku <kws1044@gmail.com>
2023-04-26 12:19:14 +0900

    aop logger 추가

--- jwland-api/src/main/java/com/jwland/jwland/aop/LoggerAop.java
+++ jwland-api/src/main/java/com/jwland/jwland/aop/LoggerAop.java
@@ -1,2 +1,21 @@
-package com.jwland.jwland.aop;public class LoggerAop {
+package com.jwland.jwland.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Slf4j
+@Component
+public class LoggerAop {
+
+    @Before("execution(* com.jwland.jwland..*Controller.*(..))")
+    public void controllerLogger(JoinPoint joinPoint){
+        log.info("Executed controller : {}", joinPoint.getSignature());
+    }
+
+
 }
